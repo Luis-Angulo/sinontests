a7e7491ae893fdc003bf1808d27e829fadfe00c9
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2020-03-19 12:49:44 -0700

    spying on functions - creating a spy

--- file.management.spy.spec.js
+++ file.management.spy.spec.js
@@ -4,8 +4,25 @@ const fs = require("fs");
 const proxyquire = require("proxyquire");
 const fileManagement = require("./file.management");
 
+// Spy example
+// Component to test
 describe("File Management", () => {
-  it("true should be true", () => {
-    expect(true).to.be.true;
+  // functionality to test
+  describe("When writing a new file", () => {
+    // test case
+    it("should call writeFileSync", () => {
+      /* arrange */
+      const writeSpy = spy(fs, "writeFileSync");
+      const writeArgs = "";  // a txt file is created empty by default if no args are passed
+
+      /* act */
+      fileManagement.createFile("test.txt");
+      
+      /* assert */
+      expect(writeSpy.calledWith("./data/test.txt", writeArgs))
+      .to.be.true;  // should pass, but it doesnt, explained in "using a spy"
+    });
+    
   });
+
 });
